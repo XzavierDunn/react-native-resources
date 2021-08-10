5217a50ffb9741561e40d17a5a8a64416f36061d
XD <xmandunn@gmail.com>
2021-08-10 19:56:16 -0400

    first commit

---
+++ start/App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>lk;fdsj;lkfjas;lkdjas;ldkjf;alskddfj;lkj</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
